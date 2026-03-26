2ef36fd5555d0bdc49a9a11584e7b6a9f6c9febd
rafaelvrshots-hue <rafaelvrshots@gmail.com>
2026-03-25 19:58:48 -0500

    Create pages/index.js with a simple React component for Next.js

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const IndexPage = () => {
+    return (
+        <div>
+            <h1>Welcome to Anzoategui Smart!</h1>
+            <p>This is the homepage for your Next.js application.</p>
+        </div>
+    );
+};
+
+export default IndexPage;
